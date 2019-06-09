1c9b9d09f03a411dc6a7461ebbbcd4ac5938129a
Sarvesh Tandon <sarveshtandon100@gmail.com>
2019-06-09 15:45:14 +0530

    All done except for polishing the app and adding notifications features

--- app/src/main/java/sarveshtandon/www/community/MainActivity.java
+++ app/src/main/java/sarveshtandon/www/community/MainActivity.java
@@ -1,27 +1,41 @@
 package sarveshtandon.www.community;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     Button createCommunity , enterCommunity;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private static final int RC_SIGN_IN = 1;
+    public String username;
+    private static final String ANONYMOUS = "Guest";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mFirebaseAuth = FirebaseAuth.getInstance();
         createCommunity = (Button) findViewById(R.id.button);
         enterCommunity = (Button) findViewById(R.id.button2);
         createCommunity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), CreateCommunityActivity.class));
+                //startActivity(new Intent(getApplicationContext(), CreateCommunityActivity.class));
+                AuthUI.getInstance().signOut(getApplicationContext());
             }
         });
         enterCommunity.setOnClickListener(new View.OnClickListener() {
@@ -30,7 +44,70 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),EnterCommunity.class));
             }
         });
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
 
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Toast.makeText(MainActivity.this, "You're now signed in. Welcome to Community.", Toast.LENGTH_SHORT).show();
+                    username = user.getDisplayName();
+                } else {
+                    // User is signed out
+                    username = ANONYMOUS;
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .setLogo(R.mipmap.ic_launcher)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+
+            }
+        };
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Sign-in succeeded, set up the UI
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                username = user.getDisplayName();
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // Sign in was canceled by the user, finish the activity
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+    @Override
+    public void onPause() {
+
+        super.onPause();
+        if(mAuthStateListener!=null)
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        detachDatabaseReadListener();
+    }
+
+    private void detachDatabaseReadListener() {
+        username = ANONYMOUS;
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 
 //TODO: Add notifications feature
